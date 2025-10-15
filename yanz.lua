91ea6eb83ba30f8e8166b7d36242d87a67067c6a
YanzXNexuz666xZ <bibityasin49@gmail.com>
2025-10-15 21:33:09 +0700

    Update yanz.lua

--- yanz.lua
+++ yanz.lua
@@ -9188,8 +9188,8 @@ UniversalTab:AddButton({
 
 UniversalTab:AddButton({
     Name = "Fe emote",
-    Callback = function()
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Gi7331/scripts/main/Emote.lua"))()
+    Callback = function()        
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Gazer-Ha/Free-emote/refs/heads/main/Delta%20mad%20stuffs"))()
     end
 })
 
